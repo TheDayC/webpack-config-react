aa6def8fc8f22fd6ba0a5e04b4bb96732a8f01f2
David Connolly <daveconnolly90@gmail.com>
2018-04-04 12:49:36 +0100

    stack-complete

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'; // Start our app away from root render. Keeps the app tidy.
+
+ReactDOM.render(<App />, document.getElementById('root'));
